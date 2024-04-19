8b48a25b538ae68f67c14d109bf4d4db7128bbf3
CocAncels <corentin.ancel@edu.devinci.fr>
2024-04-19 14:29:33 +0200

    REMOVE: Snake

--- frontend/src/Components/RouterContainer.tsx
+++ frontend/src/Components/RouterContainer.tsx
@@ -6,7 +6,6 @@ import { Home } from "../Pages/Home";
 import { Login } from "../Pages/Login";
 import { Register } from "../Pages/Register";
 import { RPS } from "../Games/RPS/RPS";
-import SnakeGame from "../Games/Snake/SnakeGame";
 
 export const RouterContainer = () => {
   const token = useSelector((state: RootState) => state.token);
@@ -19,7 +18,6 @@ export const RouterContainer = () => {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/rock-paper-scissors" element={<RPS />} />
-            <Route path="/snake" element={<SnakeGame />} />
           </Routes>
         </Overlay>
       ) : (
